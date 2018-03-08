937c952c24689034a95be7c0d169f04f0ed392ec
TempUser03 <TempUser03@idgcapital.local>
2018-03-08 14:07:47 +0800

    shi

--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -5,27 +5,28 @@ import Body from '../common/Body.js'
 import Footer from '../common/Footer.js'
 
 class IndexCom extends React.Component {
-    constructor (props){
-        super (props)
-        this.state={
-            middleData:[]
-        },
-        this.transmitData = this.transmitData.bind(this)
-    }
-    transmitData (data){
-        this.setState({
-            middleData:data
-        })
-    }
-    render (){
-        return (
-            <div className='index'>
-                <Header outputData={this.transmitData}/>
-                <Body Things={this.state.middleData}/>
-                <Footer />
-            </div>
-        )
-    }
+  constructor (props){
+    super (props)
+    this.state={
+      middleData:[]
+    },
+    this.transmitData = this.transmitData.bind(this)
+  }
+  transmitData (data){
+    this.setState({
+      middleData:data
+    })
+  }
+  render (){
+    console.log('la')
+    return (
+      <div className='index'>
+        <Header outputData={this.transmitData}/>
+        <Body Things={this.state.middleData}/>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default IndexCom
