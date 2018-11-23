4938f623d0468f72b29aea3d28cce26a6f2a515e
byshi <byshi@ronglian.com>
2018-11-23 18:34:15 +0800

    shibaiyong change

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+  import React from 'react'
+  import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+  import IndexCom from '../components/index/Index.js'
+  import Detail from '../components/details/Detail.js'
+  import Mine from '../components/mine/Mine.js'
+  import List from '../components/pageList/List.js'
+  import Login from '../components/logins/Login'
+  import Register from '../components/logins/Register'
+  import Tabs from '../components/tabs/TabUI'
+  import Broadcast from '../components/common/Broadcast'
+
+  const Root = () => (
+    <Router history={browserHistory}>
+      <Route path="/" component={IndexCom}></Route>
+      <Route path='/detail' component={Detail}></Route>
+      <Route path='/tabs' component={Tabs}></Route>
+      <Route path='/list' component={List}></Route>
+      <Route path='/broadcast' component={Broadcast}></Route>
+      <Route path='/mine' component={Mine}>
+        <IndexRoute component={Login}/> 
+        <Route path='/mine/login' component={Login}></Route>
+        <Route path='/mine/register' component={Register}></Route>
+      </Route>
+    </Router>
+  )
+
+  export default Root
+
+  
