6208e6ffd04df3f50c35877f2d25cd953058f34d
byshi <byshi@ronglian.com>
2018-12-10 18:27:32 +0800

    shibaiyong change

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
   import React from 'react'
-  import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+  import {Router, Route, IndexRoute,BrowserRouter} from 'react-router-dom'
   import IndexCom from '../components/index/Index.js'
   import Detail from '../components/details/Detail.js'
   import Mine from '../components/mine/Mine.js'
@@ -9,19 +9,30 @@
   import Tabs from '../components/tabs/TabUI'
   import Broadcast from '../components/common/Broadcast'
 
+  // const Root = () => (
+  //   <Router history={browserHistory}>
+  //     <Route path="/" component={IndexCom}></Route>
+  //     <Route path='/detail' component={Detail}></Route>
+  //     <Route path='/tabs' component={Tabs}></Route>
+  //     <Route path='/list' component={List}></Route>
+  //     <Route path='/broadcast' component={Broadcast}></Route>
+  //     <Route path='/mine' component={Mine}>
+  //       <IndexRoute component={Login}/> 
+  //       <Route path='/mine/login' component={Login}></Route>
+  //       <Route path='/mine/register' component={Register}></Route>
+  //     </Route>
+  //   </Router>
+  // )
   const Root = () => (
-    <Router history={browserHistory}>
+    <BrowserRouter>
+      
       <Route path="/" component={IndexCom}></Route>
       <Route path='/detail' component={Detail}></Route>
       <Route path='/tabs' component={Tabs}></Route>
       <Route path='/list' component={List}></Route>
       <Route path='/broadcast' component={Broadcast}></Route>
-      <Route path='/mine' component={Mine}>
-        <IndexRoute component={Login}/> 
-        <Route path='/mine/login' component={Login}></Route>
-        <Route path='/mine/register' component={Register}></Route>
-      </Route>
-    </Router>
+      
+    </BrowserRouter>
   )
 
   export default Root
